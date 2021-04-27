f50819e59dd0be1f1553be7283ac1d221cbfbff7
Parimal Chandwaskar <Parimal.Chandwaskar@wu.com>
2021-04-28 00:15:06 +0530

    Adding Router functionality placholder using withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import HomePage from '../src/pages/homepage/homepage.component';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
+
+const HatsPage = (props) => (
+console.log(props),
+<div>
+    <h1> HATS PAGE RENDERED</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route path='/hats' component={HatsPage}></Route>
+      </Switch>
     </div>
   );
 }
