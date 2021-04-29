c5737ae0803c67d834f1a3da375a4ad7695675ba
Parimal Chandwaskar <Parimal.Chandwaskar@wu.com>
2021-04-29 12:24:18 +0530

    Created a SIGN In Page and added components for Sign In form, Input Form and Custom Button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Route, Switch} from 'react-router-dom';
 import HomePage from '../src/pages/homepage/homepage.component';
 import ShopPage from '../src/pages/shop/shop.component';
 import Header from '../src/components/header/header.component';
+import SignInAndSignUpPage from '../src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route path='/shop' component={ShopPage}></Route>
+        <Route path='/signin' component={SignInAndSignUpPage}></Route>
       </Switch>
     </div>
   );
