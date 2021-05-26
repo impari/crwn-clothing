335bded0242dbd9144dfdd5e958ea1595a8d89db
Parimal Chandwaskar <parimal.chandwaskar@wu.com>
2021-05-26 13:35:48 +0530

    Added cartIcon & cartDropdown components also created a Cart STORE to hide and show Cart dropdown on user clicks

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './cart-icon.styles.scss';
+import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
+import { connect } from 'react-redux';
+import { toggleCartDropdown } from '../../redux/cart/cart.actions';
+
+const CartIcon = ({toggleCartDropdown}) => (
+    console.log(toggleCartDropdown),
+    // console.log(hidden),
+    <div className='cart-icon' onClick={toggleCartDropdown}>
+        <ShoppingIcon className='shopping-icon' />
+        <span className='item-count'>0</span>
+    </div>
+);
+
+/* 
+* As the first argument passed in to connect, mapStateToProps is used for selecting the part of the data from the store that the connected component needs. 
+  It’s frequently referred to as just mapState for short.
+  It is called every time the store state changes.
+  It receives the entire store state, and should return an object of data this component needs.
+*
+*/
+const mapStateToProps = (state) => (console.log(state), {
+    hidden: state.cart.hidden
+});
+
+/*
+ * As the second argument passed in to connect, mapDispatchToProps is used for dispatching actions to the store.
+   dispatch is a function of the Redux store. You call store.dispatch to dispatch an action. 
+   This is the only way to trigger a state change.
+   Your component will receive dispatch by default, i.e., when you do not supply a second parameter to connect():
+   Your mapDispatchToProps functions are expected to return an object. 
+   Each fields of the object should be a function, calling which is expected to dispatch an action to the store.
+   The return of your mapDispatchToProps functions are regarded as dispatchProps. 
+   It will be merged as props to your connected component.
+ */
+
+const mapDispatchToProps = (dispatch) =>(console.log(toggleCartDropdown),{
+    toggleCartDropdown : () => dispatch(toggleCartDropdown())
+}); 
+
+
+export default connect(null, mapDispatchToProps)(CartIcon);
