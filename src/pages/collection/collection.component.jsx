9916f9aacd95b7f87f5428ec2f05ac6739820436
Parimal Chandwaskar <parimal.chandwaskar@wu.com>
2021-06-26 17:39:25 +0530

    Creating a Collection selector for individual collection items, pass it into collection component

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import './collection.styles.scss';
+import { selectCollection } from '../../redux/shop/shop.selectors';
 
-
-const CollectionPage = ({match}) => {
-    console.log(match);
+const CollectionPage = ({collection}) => {
+    console.log(collection);
     return (
         <div className='collection-page'>
             <h2> collection Page</h2>
@@ -11,4 +13,22 @@ const CollectionPage = ({match}) => {
     );
 }
 
-export default CollectionPage;
+/**
+ * 
+ * @param {*} state 
+ * @param {*} ownProps 
+ * Since mapStateToProps return an Object this can use ShortHand as below. 
+ * Common practice is to use short hand
+ * For my understanding I have actually wrote full function
+ * Below (here in comments a ShortHand code): 
+            const mapStateToProps = (state, ownProps) => ({
+                collection: selectCollection(ownProps.match.params.collectionId)(state)
+            });
+ */
+const mapStateToProps = (state, ownProps) => {
+    return {
+        collection: selectCollection(ownProps.match.params.collectionId)(state)
+    };
+}
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,4 +1,13 @@
 import { createSelector } from 'reselect';
+import memoize from 'lodash.memoize';
+
+const COLLECTION_ID_MAP = {
+    hats: 1,
+    sneakers: 2,
+    jackets: 3,
+    womens: 4,
+    mens: 5
+};
 
 const selectShop = state => state.shop;
 
@@ -6,3 +15,28 @@ export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 );
+
+/**
+ * This is only to select single collection from CollectionS of items
+ * 
+ * @param {*} collectionUrlParam 
+ * Below is the short Hand method to write selector function
+ * 
+        export const selectCollection = (collectionUrlParam) => 
+         createSelector(
+            [selectCollections],
+            collections =>  collections.find(
+                collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+            )
+        )
+ * 
+ */
+
+export const selectCollection = memoize((collectionUrlParam) => {
+   return createSelector(
+        [selectCollections],
+        collections => {
+            return collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+        }
+    )
+});
