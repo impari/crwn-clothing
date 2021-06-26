12c67835d54759c9cc9d2f9f5963f1aad1e35285
Parimal Chandwaskar <parimal.chandwaskar@wu.com>
2021-06-26 12:13:09 +0530

    moved the code from Shop page to collection overview page, Added nested Routes on Shop Page, created coolection Page

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './collection.styles.scss';
+
+
+const CollectionPage = ({match}) => {
+    console.log(match);
+    return (
+        <div className='collection-page'>
+            <h2> collection Page</h2>
+        </div>
+    );
+}
+
+export default CollectionPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,31 +1,18 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import { Route } from 'react-router-dom';
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+import CollectionsOverview from '../../components/collection-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({collections}) => {
+const ShopPage = ({match}) => {
+       console.log(match);
         return(
             <div className='shop-page'>
-                {collections.map(({id, ...otherColecctionProps}) => (
-                    <CollectionPreview key={id} {...otherColecctionProps} />
-                ))}
+                <Route exact path={`${match.path}`} component={CollectionsOverview} />
+                <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
             </div>
             
         );
 }
 
-/* Way to USE mapStateToProps without "reselect"
- * 
-  const mapStateToProps = ({shop}) => ({
-    collections: shop.collections
-  });
- *
- */ 
-
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
