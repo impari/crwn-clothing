7aaa6b9a2e5ed7a0810688dd0b19513e5d45cb57
Parimal Chandwaskar <parimal.chandwaskar@wu.com>
2021-07-06 11:56:31 +0530

    Added styled component and changes to Homepage component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,17 @@
+/**
+ * Used Styled component here
+ */
+
 import React from 'react';
-import './homepage.styles.scss';
+//import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component'
+import {HomePageContainer} from './homepage.styles';
 
 const HomePage = () => {
     return (
-        <div className='homepage'>
+        <HomePageContainer>
             <Directory />
-        </div>
+        </HomePageContainer>
     );
 }
 
---
+++ src/pages/homepage/homepage.styles.jsx
@@ -0,0 +1,13 @@
+/**
+ * This is a Styled componenent
+ * Replaced a CSS from a original file to here
+ */
+
+import styled from 'styled-components';
+
+export const HomePageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px 80px;
+`;
